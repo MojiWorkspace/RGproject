60169ae09c19c79c4f9b0973d7e7d692991f2919
Maksim Marfey <mmarfey@get-bc.local>
2024-06-14 16:12:26 +0300

    Initial project settings and comments

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,93 @@
+﻿using Melanchall.DryWetMidi.Interaction;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    public KeyCode input;
+    public GameObject notePrefab;
+    List<Note> notes = new List<Note>();
+    public List<double> timeStamps = new List<double>(); // список таймингов нот в миди файле
+    public List<double> notesLen = new List<double>(); // список длин нот из миди файла
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    void Start()
+    {
+
+    }
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        TempoMap tempoMap = SongManager.midiFile.GetTempoMap();
+
+        foreach (var note in array)
+        {
+            if (note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, tempoMap);
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+
+                //временный хардкод для получения длины ноты в виде числа (1 - обычная нота, все, что больше - нота с удержанием, потом на это число умножаем скейл по Y)
+                var noteLength = note.LengthAs<MetricTimeSpan>(tempoMap).TotalMicroseconds / 1000 / 107;
+                notesLen.Add(noteLength);
+            }
+        }
+    }
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        {
+            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.Instance.noteTime)
+            {
+                //спавним очередную ноту и присваиваем ей значения параметров
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<Note>());
+                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex];
+                note.GetComponent<Note>().assignedLength = (float)notesLen[spawnIndex];
+                spawnIndex++;
+            }
+        }
+
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.Instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelayInMilliseconds / 1000.0);
+
+            //обрабаботка ввода
+            if (Input.GetKeyDown(input))
+            {
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on {inputIndex} note");
+                    Destroy(notes[inputIndex].gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit inaccurate on {inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                }
+            }
+            if (timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Missed {inputIndex} note");
+                inputIndex++;
+            }
+        }
+
+    }
+    private void Hit()
+    {
+        ScoreManager.Hit();
+    }
+    private void Miss()
+    {
+        ScoreManager.Miss();
+    }
+}
---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    double timeInstantiated;
+    public float assignedTime;
+    public float assignedLength;
+    private float changeY = 0;
+    void Start()
+    {
+        timeInstantiated = SongManager.GetAudioSourceTime();
+
+        // Если нота с удержанием, то вычисяем изначальное смещение по Y и новый скейл по Y
+        // if (assignedLength > 1) {
+        //     Vector3 baseScale = transform.localScale;
+
+        //     changeY = baseScale.y * (assignedLength - 1);
+        //     baseScale.y *= assignedLength;
+        //     transform.localScale = baseScale;
+        // }
+
+    }
+
+    void Update()
+    {
+        double timeSinceInstantiated = SongManager.GetAudioSourceTime() - timeInstantiated;
+        // t параметр изменяется от 0 до 1 в Lerp()
+        float t = (float)(timeSinceInstantiated / (SongManager.Instance.noteTime *2));
+
+        if (t > 1)
+        {
+            // если достигли координаты деспавна, то уничтожаем объект
+            Destroy(gameObject);
+        }
+        else
+        {
+            // перемещаем ноту от точки спавна (со смещением по Y) до точки деспавна
+            transform.localPosition = Vector3.Lerp(Vector3.up * SongManager.Instance.noteSpawnY + new Vector3(0, changeY, 0), Vector3.up * SongManager.Instance.noteDespawnY, t);
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+}
