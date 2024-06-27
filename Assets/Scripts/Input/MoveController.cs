1f64a5f65504feeed394c046901966d892521d9b
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2024-06-27 13:53:29 +0300

    Создал InputManager, MoveController, MoveConfig, Character + сделал сцену и чутка настроил ее

---
+++ Assets/Scripts/Input/MoveController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace RhythmGame
+{
+    public sealed class MoveController : MonoBehaviour
+    {
+        [SerializeField] private Character _character;
+        [SerializeField] private InputManager _inputManager;
+
+        void Update()
+        {
+            _character.Move(_inputManager.Move(), Time.deltaTime);
+        }
+    }
+}
